2c5da8be0f7f6eff2ae767e212b9b78a4a9d5a22
Бутенко Антон <a.butenko@ESGN.LOCAL>
2021-02-25 11:05:01 +0200

    add prod files

---
+++ config/params.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'adminEmail' => 'admin@example.com',
+    'senderEmail' => 'noreply@example.com',
+    'senderName' => 'Example.com mailer',
+ //   'BaseFilePath'=>'/home/soft/public_html/prst.in.ua/files',
+
+    'BaseFilePath'=>'C:\OSPanel\domains\prst.local\web\files',
+];
---
+++ config/web.php
@@ -0,0 +1,109 @@
+<?php
+
+$params = require __DIR__ . '/params.php';
+$db = require __DIR__ . '/db.php';
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+	'language' => 'uk-UK',
+    'name'=>'You name',
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'modules' => [
+        'gridview' => [
+            'class' => 'kartik\grid\Module',
+            // other module settings
+        ]
+    ],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'F0-QU3Q87rAlqJEzz88VgxzQs9IsOABx',
+        ],
+
+	   'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',  // e.g. smtp.mandrillapp.com or smtp.gmail.com
+                'username' => 'Mail@gmail.com',
+                'password' => 'pass',
+                'port' => '465', // Port 25 is a very common port too
+                'encryption' => 'ssl', // It is often used, check your provider or mail server specs
+            ],
+        ],
+
+        'imap' => [
+            'class' => 'kekaadrenalin\imap\Imap',
+            'connection' => [
+              'imapPath' => '{imap.gmail.com:993/imap/ssl}INBOX',
+              'imapLogin' => 'mail@gmail.com',
+              'imapPassword' => 'pass',
+              'serverEncoding'=>'encoding', // utf-8 default.
+              //'attachmentsDir'=>'/'
+			  'attachmentsDir'=>'/home/soft/public_html/prst.in.ua/files/mail_attached/'
+			 // 'decodeMimeStr'  => true,
+            ],
+        ],
+
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+		  ''=>'main/index',
+        	'<controller>/<action>'=> '<controller>/<action>'
+            ],
+        ],
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        'allowedIPs' => ['127.0.0.1', '::1', '192.168.33.180'],
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        'allowedIPs' => ['127.0.0.1', '::1', '192.168.33.180', '192.168.33.204'],
+    ];
+}
+
+return $config;
